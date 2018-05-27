74020c281ea851cc2888ac883f1517abf7827140
Tim Cussins <volumedigital@eml.cc>
2018-05-27 11:02:42 +0100

    Cleanup landing page before commit :)

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,12 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        ts2
-      </h1>
-      <h2 class="subtitle">
+      <h1 class="subtitle">
         Tired Souls
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      </h1>
     </div>
   </section>
 </template>
-
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
 
 <style>
 .container {
